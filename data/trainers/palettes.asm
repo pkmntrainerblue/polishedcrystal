ab73bd68fc66b4f82c8f190b302997b0227199b9
pkmntrainerblue <combats-jicama.6c@icloud.com>
2025-08-24 22:41:00 +0300

    Update palettes.asm

--- data/trainers/palettes.asm
+++ data/trainers/palettes.asm
@@ -6,7 +6,7 @@ if !DEF(MONOCHROME)
 KrisPalette:
 INCLUDE "gfx/trainers/carrie.pal"
 ChrisPalette:
-INCLUDE "gfx/trainers/rich_boy.pal"
+INCLUDE "gfx/trainers/cal.pal"
 CrysPalette:
 INCLUDE "gfx/trainers/jacky.pal"
 INCLUDE "gfx/trainers/falkner.pal"

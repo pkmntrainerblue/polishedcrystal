9aa85a8ff0add5e022b7898b09deb6c2e5a710f0
pkmntrainerblue <combats-jicama.6c@icloud.com>
2025-08-28 18:02:30 +0300

    Update extras.asm

--- gfx/pokemon/extras.asm
+++ gfx/pokemon/extras.asm
@@ -439,3 +439,6 @@ TaurosPaldeanWaterAnimationExtra: INCLUDE "gfx/pokemon/tauros_paldean_water/anim
 
 UrsalunaPlainAnimationExtra:     INCLUDE "gfx/pokemon/ursaluna_plain/anim_idle.asm"
 UrsalunaBloodmoonAnimationExtra: INCLUDE "gfx/pokemon/ursaluna_bloodmoon/anim_idle.asm"
+
+PikachuTrainerRedAnimationExtra:   INCLUDE "gfx/pokemon/pikachu_pika/anim_idle.asm"
+PikachuTrainerYellowAnimationExtra: INCLUDE "gfx/pokemon/pikachu_chuchu/anim_idle.asm"

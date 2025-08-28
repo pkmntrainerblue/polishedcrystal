7edc58ad63934d85fff2a677f264a60dca0c1b50
pkmntrainerblue <combats-jicama.6c@icloud.com>
2025-08-28 15:43:43 +0300

    Update PlayersHouse2F.asm

--- maps/PlayersHouse2F.asm
+++ maps/PlayersHouse2F.asm
@@ -250,8 +250,8 @@ endr
 	loadmem wPartyMon3Shiny, SHINY_MASK
 	loadmem wPartyMon3PokerusStatus, POKERUS_CURED
 	; ext species test
-	givepoke URSALUNA, URSALUNA_BLOODMOON_FORM, 50
-	givepoke DUDUNSPARCE, DUDUNSPARCE_THREE_SEGMENT_FORM, 50
+	givepoke PIKACHU, PIKACHU_TRAINER_RED_FORM, 50
+	givepoke PIKACHU, PIKACHU_TRAINER_YELLOW_FORM, 50
 	; evolve during battle
 	givepoke PUPITAR, 54
 	loadmem wPartyMon6Exp+2, LOW(207967)

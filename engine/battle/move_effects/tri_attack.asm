65d7488bf96e1e3250a244d847b2babe0c544b37
pkmntrainerblue <combats-jicama.6c@icloud.com>
2025-10-08 14:01:05 +0300

    Update tri_attack.asm

--- engine/battle/move_effects/tri_attack.asm
+++ engine/battle/move_effects/tri_attack.asm
@@ -1,26 +1,9 @@
-    BattleCommand_tristatuschance:
-	call BattleCheckTypeMatchup
-	ld a, [wTypeMatchup]
-	ld b, a
-	ldh a, [hBattleTurn]
-	and a
-	ld a, [wEnemyMonType1]
-	jr z, .check_type1
-	ld a, [wBattleMonType1]
-.check_type1
-	cp WATER
-	jr z, .make_super_effective
-	ldh a, [hBattleTurn]
-	and a
-	ld a, [wEnemyMonType2]
-	jr z, .check_type2
-	ld a, [wBattleMonType2]
-.check_type2
-	cp WATER
-	jr z, .make_super_effective
-	ret
-.make_super_effective
-	ld a, SUPER_EFFECTIVE
-	ld [wTypeMatchup], a
-    ret
-   
+BattleCommand_tristatuschance:
+	ld a, 3
+	call BattleRandomRange
+	call StackJumpTable
+
+.Jumptable:
+	dw BattleCommand_paralyzetarget ; paralyze
+	dw BattleCommand_freezetarget ; freeze
+	dw BurnTarget ; burn, no defrosting

9f686dfa290f0b82deb960932bea6c7e1742a08b
pkmntrainerblue <combats-jicama.6c@icloud.com>
2025-10-08 13:44:54 +0300

    Update thunder.asm

--- engine/battle/move_effects/thunder.asm
+++ engine/battle/move_effects/thunder.asm
@@ -1,15 +1,26 @@
-BattleCommand_thunderaccuracy:
-	ld a, BATTLE_VARS_MOVE_TYPE
-	call GetBattleVarAddr
-	inc hl
-	call GetWeatherAfterOpponentUmbrella
-	cp WEATHER_RAIN
-	jr z, .rain
-	cp WEATHER_SUN
-	ret nz
-	ld [hl], 50 percent + 1
+    BattleCommand_FreezeDry:
+	call BattleCheckTypeMatchup
+	ld a, [wTypeMatchup]
+	ld b, a
+	ldh a, [hBattleTurn]
+	and a
+	ld a, [wEnemyMonType1]
+	jr z, .check_type1
+	ld a, [wBattleMonType1]
+.check_type1
+	cp WATER
+	jr z, .make_super_effective
+	ldh a, [hBattleTurn]
+	and a
+	ld a, [wEnemyMonType2]
+	jr z, .check_type2
+	ld a, [wBattleMonType2]
+.check_type2
+	cp WATER
+	jr z, .make_super_effective
 	ret
-
-.rain
-	ld [hl], 100 percent
-	ret
+.make_super_effective
+	ld a, SUPER_EFFECTIVE
+	ld [wTypeMatchup], a
+    ret
+   

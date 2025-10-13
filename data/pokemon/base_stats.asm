7c42358e349a5b071c3364d9ffa9a266f3b16f69
pkmntrainerblue <combats-jicama.6c@icloud.com>
2025-10-13 11:48:19 +0300

    Update base_stats.asm

--- data/pokemon/base_stats.asm
+++ data/pokemon/base_stats.asm
@@ -382,4 +382,7 @@ INCLUDE "data/pokemon/base_stats/tauros_paldean_water.asm"
 
 INCLUDE "data/pokemon/base_stats/ursaluna_bloodmoon.asm"
 
+INCLUDE "data/pokemon/base_stats/pikachu_partner.asm"
+
+
 	assert_table_length NUM_EXT_POKEMON

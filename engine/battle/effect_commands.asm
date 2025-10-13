4664c2643fa048018692303886d35bfe44698ccc
pkmntrainerblue <combats-jicama.6c@icloud.com>
2025-10-13 11:27:20 +0300

    Update effect_commands.asm

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1664,35 +1664,7 @@ CheckAirborne_GotAbility:
 	ret
 
 BattleCheckTypeMatchup:
-    ld a, BATTLE_VARS_MOVE_EFFECT
-	call GetBattleVar
-	cp EFFECT_FREEZE_DRY
-	jr nz, .normal_matchup
-	ldh a, [hBattleTurn]
-	and a
-	ld a, [wEnemyMonType1]
-	jr z, .check_type1
-	ld a, [wBattleMonType1]
-.check_type1
-	cp WATER
-	jr z, .super_effective
-	ldh a, [hBattleTurn]
-	and a
-	ld a, [wEnemyMonType2]
-	jr z, .check_type2
-	ld a, [wBattleMonType2]
-.check_type2
-	cp WATER
-	jr z, .super_effective
-	jr .normal_matchup
-.super_effective
-	ld a, SUPER_EFFECTIVE
-	ld [wTypeMatchup], a
-	ret
-.normal_matchup
-	; freeze dry over
-
-	ldh a, [hBattleTurn]
+    ldh a, [hBattleTurn]
 	and a
 	ld hl, wEnemyMonType1
 	jr z, CheckTypeMatchup

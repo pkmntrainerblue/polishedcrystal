f920e0bba21d099f71270aca0922913a3d89bb00
pkmntrainerblue <combats-jicama.6c@icloud.com>
2025-10-20 16:49:55 +0300

    Update anim.asm

--- gfx/pokemon/pikachu_partner/anim.asm
+++ gfx/pokemon/pikachu_partner/anim.asm
@@ -1,11 +1,9 @@
-	frame 1, 07
-	setrepeat 2
-	frame 2, 07
-	frame 3, 07
-	dorepeat 2
-	frame 2, 26
-	setrepeat 2
-	frame 0, 05
-	frame 4, 05
-	dorepeat 7
-	endanim
+	frame 1, 10
+    frame 2, 10
+    frame 1, 10
+    frame 2, 10
+    frame 1, 10
+    frame 2, 10
+    frame 1, 10
+    frame 2, 10
+    endanim

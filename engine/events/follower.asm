8b56057567e4e1b075b13a8be7f017d921c2a4dc
Blue <combats-jicama.6c@icloud.com>
2025-10-31 16:57:00 +0200

    Update follower.asm

--- engine/events/follower.asm
+++ engine/events/follower.asm
@@ -4,27 +4,60 @@ FollowerScript::
 	faceplayer
 	cry PIKACHU
     special GetFirstPokemonHappiness
-	ifgreater $f9, .LovesYouALot
+	ifgreater $f9, .AdoresYou
 	ifgreater $c7, .ReallyTrustsYou
-	ifgreater $95, .KindaHappyWithYou
+	ifgreater $95, .HappyWithYou
 	ifgreater $63, .CuriousAboutYou
 	ifgreater $31, .NotUsedToYou
 
-.LovesYouALot:
-    text "Love"
-    done
+.AdoresYou:
+    opentext
+    writetext .AdoresYouText
+    waitbutton
+    closetext
+    end
 
+.AdoresYouText:
+    text "Pikachu absolutely"
+    line "adores you!"
+    done
+    
 .ReallyTrustsYou:
-    text "Trust"
+    opentext
+    writetext .ReallyTrustsYouText
+    waitbutton
+    closetext
+    end
+
+.ReallyTrustsYouText:
+    text "Pikachu trusts you"
+    line "completely."
     done
 
-.KindaHappyWithYou:
-    text "Like"
+.HappyWithYou:
+    opentext
+    writetext .HappyWithYouText
+    waitbutton
+    closetext
+    end
+
+.HappyWithYouText:
+    text "Pikachu is content"
+    line "spending time with"
+    cont "you."
     done
 
 .CuriousAboutYou:
-    text "Curious"
-    done
+    opentext
+    writetext .CuriousAboutYouText
+    waitbutton
+    closetext
+    end
+
+.CuriousAboutYouText:
+    text "Pikachu appears to"
+    line "be watching you"
+    cont "closely."
 
 .NotUsedToYou:
     applymovement FOLLOWER, .turnback
